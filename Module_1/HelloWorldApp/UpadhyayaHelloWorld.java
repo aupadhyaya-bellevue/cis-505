e45a799d405e0aab7be7aff04a4f11f920eede4c
Abhishek Upadhyaya <aupadhyaya.osv@fedex.com>
2023-03-19 16:31:20 -0500

    Assignment 1.2 - Git setup

---
+++ Module_1/HelloWorldApp/UpadhyayaHelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * CIS505-T301 Intermediate Java Programming (2235-1)
+ * Assignment 1.2
+ * @Author Abhishek Upadhyaya
+ */
+
+public class UpadhyayaHelloWorld {
+    public static void main(String[] args) {
+        // Declare variables
+        String firstName = "Abhishek";
+        String lastName = "Upadhyaya";
+
+        System.out.printf("Hello World from %s %s", firstName, lastName);
+    }
+}

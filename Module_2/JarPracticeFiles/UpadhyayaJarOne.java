25d0f5b9e3e6aa79f088bc200d272fbe24e74ffe
Abhishek Upadhyaya <aupadhyaya.osv@fedex.com>
2023-03-26 21:40:36 -0500

    Assignment 2.2 - Jar Files

---
+++ Module_2/JarPracticeFiles/UpadhyayaJarOne.java
@@ -0,0 +1,5 @@
+public class UpadhyayaJarOne {
+    public static void main(String[] args) {
+        System.out.println("In UpadhyayaJarOne");
+    }
+}
---
+++ Module_2/JarPracticeFiles/UpadhyayaJarTwo.java
@@ -0,0 +1,5 @@
+public class UpadhyayaJarTwo {
+    public static void main(String[] args) {
+        System.out.println("In UpadhyayaJarTwo");
+    }
+}
